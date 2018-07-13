eaae68e09a7cddb47f2da7c9fbe9ca3c93a17eb0
Diego Munhoz <diego.fatec@hotmail.com>
2018-07-13 11:20:15 -0300

    Projeto Truco Penal - Game Multiplayer

---
+++ config.py
@@ -0,0 +1,15 @@
+# *********************************************************************************************
+# ******   FATEC - Faculdade de Tecnologia de Carapicuiba                                ******
+# ******   Funcao: responsavel por armazenar as configuracoes basicas de tamanho e       ******
+# ******           titulo da tela.                                                       ******
+# ******   Nome..: Diego Vinicius de Mello Munhoz    RA: 1430961723002                   ******
+# ******           Thiago da Silva Zacarias          RA: 143096172XXXX                   ******
+# ******           Victor Otavio Ponciano            RA: 143096172XXXX                   ******
+# ******   Data..: 07/07/2018                                                            ******
+# *********************************************************************************************
+
+ALTURA = 800
+LARGURA = 600
+TAMANHO_TELA = (ALTURA, LARGURA)
+TITULO = 'Truco Penal'
+FPS = 30
